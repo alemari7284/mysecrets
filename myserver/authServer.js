3b666afd46b1a92d4c728b7340e663786ddca55a
alemari7284 <alemari7284@gmail.com>
2023-01-25 03:53:19 +0100

    update project

--- myserver/authServer.js
+++ myserver/authServer.js
@@ -1,84 +1,108 @@
-const express = require("express")
-const mongoose = require("mongoose");
-const bcrypt = require("bcrypt")
-const saltRounds = 10;
+const express = require('express')
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const saltRounds = 10
 
-const cookieParser = require("cookie-parser")
-const session = require("express-session");
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
 
-const cors = require("cors");
-const app = express();
-const jwt = require("jsonwebtoken");
+const cors = require('cors')
+const app = express()
+const jwt = require('jsonwebtoken')
 
-app.use(express.json()); //replaces body-parser
+app.use(express.json()) //replaces body-parser
 app.use(cors())
 
 const userSchema = new mongoose.Schema({
-    username: String,
-    password: String
+  username: String,
+  password: String,
 })
 
-const User = new mongoose.model("User", userSchema);
+const User = new mongoose.model('User', userSchema)
 
+mongoose.connect('mongodb://localhost:27017/userDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
 
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true });
-
-
-app.post("/login", (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-    User.findOne({username}, (err, foundUser) => {
-        if (err) {
-            res.status(400);
+app.post('/login', (req, res) => {
+  const username = req.body.username
+  const password = req.body.password
+  User.findOne({ username }, (err, foundUser) => {
+    if (err) {
+      res.status(400)
+    } else {
+      console.log(foundUser)
+      // res.status(200).send(foundUser);
+      bcrypt.compare(password, foundUser.password, (error, response) => {
+        if (response) {
+          const token = jwt.sign(
+            { username: foundUser.username },
+            'jwtSecret',
+            { expiresIn: '35s' },
+          )
+          const refreshToken = jwt.sign(
+            { username: req.body.username },
+            'jwtSecret',
+          )
+          res.json({
+            auth: true,
+            message: 'successfully logged',
+            token,
+            refreshToken,
+          })
         } else {
-            console.log(foundUser);
-            // res.status(200).send(foundUser);
-            bcrypt.compare(password, foundUser.password, (error, response) => {
-                if (response) {
-                    const token = jwt.sign({username: foundUser.username}, "jwtSecret", { expiresIn: "35s"})
-                    const refreshToken = jwt.sign({username: req.body.username}, "jwtSecret")
-                    res.json({auth: true, message: "successfully logged", token, refreshToken})
-                } else {
-                    res.status(403).send("Wrong username/password combination");
-                }
-            })
+          res.status(403).send('Wrong username/password combination')
         }
-    });
-});
-
-app.post("/token", (req, res) => {
-    const refreshToken = req.body.refreshToken;
-    jwt.verify(refreshToken, "jwtSecret", (err, result) => {
-        if (err) return res.sendStatus(401)
-        const token = jwt.sign({username: req.body.username}, "jwtSecret", {expiresIn: "35s"});
-        res.json(token);
-    })
+      })
+    }
+  })
 })
 
-app.post("/register", (req, res) => {
-    console.log(req.body)
-    bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
-        if (err) {
-            console.log(err);
-        }
-        const user = new User({
-            username: req.body.username,
-            password: hash
-        })
-
-        user.save((err) => {
-            if (err) {
-                res.json({registered: false, message: err})
-            } else {
-                const token = jwt.sign({username: req.body.username}, "jwtSecret", { expiresIn: "35s"})
-                const refreshToken = jwt.sign({username: req.body.username}, "jwtSecret")
-                res.json({registered: true, message: "successfully registered", token, refreshToken})
-            }
-        })
+app.post('/token', (req, res) => {
+  const refreshToken = req.body.refreshToken
+  jwt.verify(refreshToken, 'jwtSecret', (err, result) => {
+    if (err) return res.sendStatus(401)
+    const token = jwt.sign({ username: req.body.username }, 'jwtSecret', {
+      expiresIn: '35s',
     })
+    res.json(token)
+  })
 })
 
+app.post('/register', (req, res) => {
+  console.log(req.body)
+  bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+    if (err) {
+      console.log(err)
+    }
+    const user = new User({
+      username: req.body.username,
+      password: hash,
+    })
+
+    user.save((err) => {
+      if (err) {
+        res.json({ registered: false, message: err })
+      } else {
+        const token = jwt.sign({ username: req.body.username }, 'jwtSecret', {
+          expiresIn: '35s',
+        })
+        const refreshToken = jwt.sign(
+          { username: req.body.username },
+          'jwtSecret',
+        )
+        res.json({
+          registered: true,
+          message: 'successfully registered',
+          token,
+          refreshToken,
+        })
+      }
+    })
+  })
+})
 
 app.listen(4000, () => {
-    console.log("Server running on port 4000");
+  console.log('Server running on port 4000')
 })
